22fd05d782cdb80b1e1de0be91f2bc70d8334813
mah-m <mahta.mehdiabadi@studenti.unipd.it>
2022-11-22 15:02:05 +0100

    changed the parameters to consider public reference dir instead of golden refrences

--- validation/validation.py
+++ validation/validation.py
@@ -1,90 +1,124 @@
 from __future__ import division, print_function
-import pandas as pd
-import os,json
+import os
+import json
 import sys
 from argparse import ArgumentParser
 from JSON_templates import JSON_templates
+import re
 
-parser=ArgumentParser()
-# TODO: make the help phrases better
-parser.add_argument("-i","--participant_data",help="list of the predictions",required=True)
-parser.add_argument("-com","--community_name",help="name of benchmarking community",required=True)
-parser.add_argument("-c","--challenges", nargs='+',help="list of challenges of disorder or binding, separated by space",required=True)
-parser.add_argument("-p","--participant_name",help="name of the tool used for the prediction",required=True)
-parser.add_argument("-r","--public_ref_dir",help="directory with the list of disordered residues",required=True)
-parser.add_argument("-o","--output",help="output path where participant JSON file will be written ",required=True)
+parser = ArgumentParser()
 
-args=parser.parse_args()
+parser.add_argument("-i", "--participant_data",help="the prediction file of the participant", required=True)
+parser.add_argument("-com", "--community_name",help="name of benchmarking community", required=True)
+parser.add_argument("-c", "--challenges", nargs='+',help="list of challenges of separated by space", required=True)
+parser.add_argument("-p", "--participant_name",help="name of the tool used for the prediction", required=True)
+# gold_standards_dir must change to public_ref_dir
+# parser.add_argument("-r", "--gold_standards_dir",help="directory with the public refrences are.", required=True)
+parser.add_argument("-r", "--public_ref_dir",help="directory with the public refrences are. For CAID, public_ref_dir is the same as gold_standards_dir.", required=True)
+
+parser.add_argument("-o", "--output", help="output path where participant JSON file will be written ", required=True)
+
+args = parser.parse_args()
+
 
 def main(args):
-    input_participant=args.participant_data
-    community=args.community_name
-    challenges=args.challenges
-    participant_name=args.participant_name
+    input_participant = args.participant_data
+    community = args.community_name
+    challenges = args.challenges
+    participant_name = args.participant_name
+    # gold_standards_dir = args.gold_standards_dir
+    # For CAID, public refrences are the same as Gold standards, because each submitted file by a participant must be checked agains all
+    # chosen refrence files challenges . For now, we copy the files in gold standards dir in the public ref dir, and keep the name in order 
+    # to be compatible with OEB VRE.
     public_ref_dir=args.public_ref_dir
-    out_path=args.output
+    out_path = args.output
 
-    # assuring that the output path exists
+    
     if not os.path.exists(os.path.dirname(out_path)):
         try:
             os.makedirs(os.path.dirname(out_path))
-            with open(out_path,mode="a"): pass
+            with open(out_path, mode="a"):
+                pass
             print("built the path")
         except OSError as exc:
-            print("OS error: {0}".format(exc) + "\nCould not create output path: " + out_path)
+            print("OS error: {0}".format(exc) +"\nCould not create output path: " + out_path)
 
-    validate_input_data(input_participant,public_ref_dir,community,challenges,participant_name,out_path)
+    # validate_input_data(input_participant, gold_standards_dir, community, challenges, participant_name, out_path)
+    validate_input_data(input_participant, public_ref_dir, community, challenges, participant_name, out_path)
 
-def validate_input_data(input_participant,public_ref_dir,community,challenges,participant_name,out_path):
-    try:
-        participant_data=pd.read_csv(input_participant,header=0,sep='\t')
-    except:
-        sys.exit("ERROR: Submitted data file {} is not in a valid format!".format(input_participant))
-    
-    
-    predicted_proteins = list(participant_data.iloc[:,0].values)
-    data_fields=['protein','resn','res','propensity','disordered']
-    submitted_fields=list(participant_data.columns.values)
 
-    validated=True
-    # for public_ref_rel in os.listdir(public_ref_dir):
-    #     public_ref = os.path.join(public_ref_dir,public_ref_rel)
-    #     if os.path.isfile(public_ref):
-    #         try:
-    #             public_ref_data = pd.read_csv(public_ref, sep='\t',
-    #                                               comment="#", header=0)
-    #             disordered_proteins = list(public_ref_data.iloc[:, 0].values)
-                
-    #             ## validate the fields of the submitted data and if the predicted genes are in the mutations file
-    #             if data_fields == submitted_fields and (set(predicted_proteins) < set(disordered_proteins)) == True:
-    #                 validated = True
-    #             else:
-    #                 print("WARNING: Submitted data does not validate against "+public_ref,file=sys.stderr)
-    #                 validated = False
-    #         except:
-    #             print("PARTIAL ERROR: Unable to properly process "+public_ref,file=sys.stderr)
-    #             import traceback
-    #             traceback.print_exc()
-    
-    
-    # TODO: data_id can be modified to have BINDING or DISORDER in it. 
+# def validate_input_data(input_participant, gold_standards_dir, community, challenges, participant_name, out_path):
+def validate_input_data(input_participant, public_ref_dir, community, challenges, participant_name, out_path):
+    validated = False
+    pattern = '>DP\w{5}'
+    pat = re.compile(pattern)
+    for challenge in challenges:
+        # challenge_path = os.path.join(gold_standards_dir, challenge+".txt")
+        challenge_path = os.path.join(public_ref_dir, challenge+".txt")
+        try:
+            with open(input_participant) as p, open(challenge_path) as r:
+                predictions = p.read()
+                predicted_proteins = pat.findall(predictions)
+                predicted_proteins_set = set(predicted_proteins)
+                reference = r.read()
+                reference_proteins = pat.findall(reference)
+                reference_proteins_set = set(reference_proteins)
+                if not predicted_proteins_set.issubset(reference_proteins_set):
+                    print("WARNING: Submitted data file {} contains proteins that are not in the {} reference file! Please check if you have chosen the challenges correctly based on Disorder or Binding types. ".format(input_participant, challenge), file=sys.stderr)
+                    validated = False
+                proteins = reference_proteins_set & predicted_proteins_set
+
+        except Exception as e:
+            print(e)
+            sys.exit("ERROR: Submitted data file {} can not be validated against choesn reference file {}!".format(input_participant,challenge))
+
+        with open(input_participant) as p, open(challenge_path) as r:
+            predictions = p.readlines()
+            predictions = [pred.strip().split() for pred in predictions]
+
+            reference = r.readlines()
+            reference = [ref.strip() for ref in reference]
+
+        for protein in proteins:
+            ref_idx = reference.index(protein)
+            seq_list = reference[ref_idx+1]
+            ref_seq_length = len(seq_list)
+
+            pred_idx = predictions.index([protein])
+
+            count = 1
+            while ((pred_idx + count + 1) < len(predictions)) and (not len(predictions[pred_idx + count + 1]) == 1):
+                count += 1
+
+            pred_seq_length = count
+
+            if not pred_seq_length == ref_seq_length:
+                validated = False
+                print("WARNING: Protein {protein} in the submitted file has a different length from protein in the {} reference! Please check if you have chosen the challenges correctly based on Disorder or Binding types.".format(protein, challenge), file=sys.stderr)
+
+            else:
+                cut = predictions[pred_idx + 1: pred_idx + pred_seq_length + 1]
+                cut_seq = [c[1] for c in cut]
+
+                if cut_seq == list(seq_list):
+                    validated = True
+                else:
+                    validated = False
+                    print("WARNING: Protein {} in the submitted file has a different sequence from the protein present in the {} reference! Please check if you have chosen the challenges correctly based on Disorder or Binding types.".format(protein, challenge), file=sys.stderr)
+
+            if validated == False:
+                sys.exit("ERROR: Submitted file {} does not validate against {} reference file. Please make sure the submitted file can be validated against all of the chosen reference files/challenges.".format(input_participant, challenge))
+
     data_id = community + ":" + participant_name + "_P"
-    print('data id is ',data_id)
-    output_json = JSON_templates.write_participant_dataset(data_id, community, challenges, participant_name, validated)
 
-    # print file
+    output_json = JSON_templates.write_participant_dataset(
+        data_id, community, challenges, participant_name, validated)
 
-    with open(out_path , 'w') as f:
-        json.dump(output_json, f, sort_keys=True, indent=4, separators=(',', ': '))
-        print(output_json)
-        print('dumped the data')
+    with open(out_path, 'w') as f:
+        json.dump(output_json, f, sort_keys=True,
+                  indent=4, separators=(',', ': '))
 
-    if validated == True:
 
-        sys.exit(0)
-    else:
-        sys.exit("ERROR: Submitted data does not validate against any reference data! Please check " + out_path)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main(args)
-
